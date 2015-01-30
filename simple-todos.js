5a1de3ee74fe70667b21aab454acdd9ef9b3299f
tekhuy <huyle20@hotmail.com>
2015-01-30 11:21:36 +0000

    created user accounts

--- simple-todos.js
+++ simple-todos.js
@@ -1,12 +1,24 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // This code only runs on the client 
-  // Template.body.helpers({
-  //   tasks: [
-  //     { text: "This is task 1" },
-  //     { text: "This is task 2" },
-  //     { text: "This is task 3" },
-  //   ]
-  // });
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
+    }
+  });
 
   Template.body.events({
     "submit .new-task": function (event) {
@@ -15,45 +27,36 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
 
       // Clear form
       event.target.text.value = "";
 
-      // prevent default form submit
+      // Prevent default form submit
       return false;
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
   Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {$set: {checked: ! this.checked }});
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
     },
     "click .delete": function () {
       Tasks.remove(this._id);
     }
   });
 
-};
-
-Tasks = new Mongo.Collection("tasks");
-
-if (Meteor.isClient) {
-  // this code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
-      // show newest tasks first
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
+  Accounts.ui.config({
+    passwordsSignupFields: "USERNAME_ONLY"
   });
-};
-
-
-
-
-
+}
 
 
 
